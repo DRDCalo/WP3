bab0393de3f6d5695d4a0e9b5c984ddd51ee02e4
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2025-11-07 10:59:32 +0100

    Add histograms for IDEA_o2 timing layer
    Add histograms to estimate the scintillation light yield of the IDEA_o2
    timing layer.

--- IDEA-DualReadout/SumCrystalsInformation.C
+++ IDEA-DualReadout/SumCrystalsInformation.C
@@ -22,15 +22,24 @@ int SumCrystalsInformation(std::string input_file) {
   TH1F SLY_th1 = TH1F("S light yield", ";Scintillation light yield [p.e./GeV]; Number of events", 1000, 0, 100000.0);
   TH1F CLY_th1 = TH1F("C light yield", ";Cerenkov light yield [p.e./GeV]; Number of events", 1000, 0, 100000.0);
 
+  TH1F TimingLayerEdep_th1 = TH1F("Timing layer Energy deposited", ";energy sum [GeV]; Number of events", 1000, 0.0, 12.0);
+  TH1F TimingLayerScounts_th1 = TH1F("Timing layer Scintillation counts", ";Scintillation counts; Number of events", 1000, 0, 1000000.0);
+  TH1F TimingLayerSLY_th1 = TH1F("Timing layer S light yield", ";Scintillation light yield [p.e./GeV]; Number of events", 1000, 0, 100000.0);
+
   // Fill the TH1 with the cell energy sum
   for (size_t i = 0; i < reader.getEvents(); ++i) {
     auto event = reader.readNextEvent();
     auto& crystals_edep = event.get<edm4hep::SimCalorimeterHitCollection>("SCEPCal_MainEdep");
     auto& crystals_Scounts = event.get<edm4hep::SimCalorimeterHitCollection>("SCEPCal_MainScounts");
     auto& crystals_Ccounts = event.get<edm4hep::SimCalorimeterHitCollection>("SCEPCal_MainCcounts");
+    auto& TimingLayer_edeps = event.get<edm4hep::SimCalorimeterHitCollection>("SCEPCal_TimingEdep");
+    auto& TimingLayer_Scounts = event.get<edm4hep::SimCalorimeterHitCollection>("SCEPCal_TimingScounts");
+
     float total_energy = 0.;
     float Scounts = 0.;
     float Ccounts = 0.;
+    float TL_total_energy = 0.;
+    float TL_Scounts = 0.;
     for (const auto& crystal_edep : crystals_edep) {
       total_energy += crystal_edep.getEnergy();
     }
@@ -40,19 +49,34 @@ int SumCrystalsInformation(std::string input_file) {
     for (const auto& C : crystals_Ccounts) {
       Ccounts += C.getEnergy();
     }
+    for (const auto& tl_edep : TimingLayer_edeps) {
+      TL_total_energy += tl_edep.getEnergy();
+    }
+    for (const auto& tl_S : TimingLayer_Scounts) {
+      TL_Scounts += tl_S.getEnergy();
+    }
+
     EnergyDeposited_th1.Fill(total_energy);
     Scounts_th1.Fill(Scounts);
     Ccounts_th1.Fill(Ccounts);
     SLY_th1.Fill(Scounts / total_energy);
     CLY_th1.Fill(Ccounts / total_energy);
+
+    TimingLayerEdep_th1.Fill(TL_total_energy);
+    TimingLayerScounts_th1.Fill(TL_Scounts);
+    TimingLayerSLY_th1.Fill(TL_Scounts / TL_total_energy);
+
     std::cout << "Energy deposited in crystals [GeV]: " << total_energy << " S counts " << Scounts << " C counts "
-              << Ccounts << std::endl;
+              << Ccounts << "Energy deposited in timing layer [GeV] " << TL_total_energy << " S counts " << TL_Scounts << std::endl;
   }
   EnergyDeposited_th1.Write();
   Scounts_th1.Write();
   Ccounts_th1.Write();
   SLY_th1.Write();
   CLY_th1.Write();
+  TimingLayerEdep_th1.Write();
+  TimingLayerScounts_th1.Write();
+  TimingLayerSLY_th1.Write();
 
   OutputFile.Close();
   return 0;
