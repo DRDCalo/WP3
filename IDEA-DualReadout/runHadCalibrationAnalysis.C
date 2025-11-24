f729012e53a1a00be538840404fe8f76c7459fb1
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2025-11-24 14:56:09 +0100

    Change theta values in calibration plots

--- IDEA-DualReadout/runHadCalibrationAnalysis.C
+++ IDEA-DualReadout/runHadCalibrationAnalysis.C
@@ -172,7 +172,7 @@ int runHadCalibrationAnalysis() {
   std::cout << "Starting calibration analysis..." << std::endl;
 
   for (const auto& pair : filesToAnalyze) {
-    double theta = pair.first;
+    double theta = pair.first + 90.0;
     std::string filename = pair.second;
 
     // Check if file exists before trying
